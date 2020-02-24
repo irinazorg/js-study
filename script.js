3afab73d6097e2049606b67a521f85be1df91c43
Irina <07ladymoon07@gmail.com>
2020-02-24 18:05:00 +0200

    Условия

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 "use: strict"; 
 
-var number = 5;
+/* var number = 5;
 var string = "Hello!";
 var symbol = Symbol();
 var boolean = true;
@@ -30,3 +30,40 @@ let isChecked = false,
     isClose = false;
 
 console.log(isChecked || !isClose);
+*/
+
+// Условия
+if (2*4 == 7) {
+    console.log("Верно!");
+} else {
+    console.log("Неверно!");
+}
+
+let num = 50;
+
+if (num < 49) {
+    console.log("Неверно!");
+} else if (num > 100) {
+    console.log("Много!");
+} else {
+    console.log("Верно!");
+}
+
+(num == 50) ?  console.log("Верно!") : console.log("Неверно!");
+
+switch (num) {
+    case num < 49:
+        console.log("Неверно!");
+        break;
+    case num > 100:
+        console.log("Много");
+        break;
+    case num > 80:
+        console.log("Все еще много");
+        break;
+    case 50:
+        console.log("Верно!");
+        break;
+    default: console.log("Что-то пошло не так");
+        break;
+}
